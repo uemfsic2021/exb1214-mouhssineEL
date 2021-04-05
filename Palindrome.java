9debf8934a042eeee693a4e8c6bd6acf3217df1a
mouhssineEL <>
2021-04-05 20:59:09 +0000

    not finished yet

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,15 @@
+import sun.security.util.Length;
+
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
+        int i , compteur;
+        int   N = s.Length();
+        for(i = 0; i <=N/2 ;i++){
+          if(s.charAt(i) == s.charAt(N-i)){compteur++;}
+        }
+        if(k == N){}
+
         return true;
     }
 }
